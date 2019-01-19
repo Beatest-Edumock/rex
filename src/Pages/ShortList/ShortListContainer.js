7f87de3c7c96f90ea48ca5476fefd7ef806fd556
harshvardhangupta <harshvardhan_gupta@yahoo.com>
2019-01-20 00:37:14 +0530

    Updated redux and short list page to work with new api

--- src/Pages/ShortList/ShortListContainer.js
+++ src/Pages/ShortList/ShortListContainer.js
@@ -93,6 +93,7 @@ class ShortList extends React.Component {
 
         return (
             <ShortListUI data={this.props.testOverview.test_attempts}
+                         isLoading={this.props.testOverview.name === undefined}
                          column_format={columnsModified.slice()}
 
             />
